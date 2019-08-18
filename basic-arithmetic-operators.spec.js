5ce2e2f12443c700bca465a3fe78ebf40f2f4edf
Darrius Wright <softwarebywright@gmail.com>
2019-08-18 11:51:37 -0600

    Complete arithemtric operators

---
+++ basic-arithmetic-operators.spec.js
@@ -0,0 +1,35 @@
+// test case description goes here
+test("the addition of two numbers will be their sum", () => {
+    // assertions go here
+    var x = 10;
+    var y = 3;
+
+    expect(x + y).toBe(13);
+})
+
+// test case description goes here
+test("the subtraction of two numbers will be their difference", () => {
+    // assertions go here
+    var x = 10;
+    var y = 3;
+
+    expect(x - y).toBe(7)
+})
+
+// test case description goes here
+test("the multiplication of two numbers will be their product", () => {
+    // assertions go here
+    var x = 10;
+    var y = 3;
+
+    expect(x * y).toBe(30)
+})
+
+// test case description goes here
+test("the division of two numbers will be their quotient", () => {
+    // assertions go here
+    var x = 10;
+    var y = 3;
+
+    expect(x / y).toBe(3.3333333333333335)
+})
